e78eab96628efa9cbc2e28630e2f2c653722ec49
aryanbhale <aryanbhale2004@gmail.com>
2025-10-04 17:22:16 +0530

    Main_push

--- js/details.js
+++ js/details.js
@@ -1,25 +1,88 @@
-function goToDetails(){
-    window.location.href = 'details.html?project=panvelstp'
-}
+// goToDetails now accepts projectId as parameter
+// document.addEventListener("DOMContentLoaded", () => {
+    function goToDetails(projectId) {
+        // window.location.href = 'details.html?project=panvelstp';
+        window.location.href = 'details.html?project=' + projectId;
+    }
 
-function getQueryParams(param){
-    const UrlParams = new URLSearchParams(window.location.search);
-    return UrlParams.get(param);
-}
+    function getQueryParams(param) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(param);
+    }
 
-if (getQueryParams("project") === "panvelstp"){
-    document.getElementById('hide').style.display = "none";
-    document.getElementById('panvelstp').style.display = "block";
+    const projectId = getQueryParams("project");
 
-}
+    document.querySelectorAll(".win").forEach(el => el.style.display = "none");
+    document.querySelectorAll(".crawler").forEach(el => el.style.display = "none");
 
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    // const projecterr = document.getElementById('hide');
+    // if (projecterr){
+    //     projecterr.style.display = "none"
+    // }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    if (projectId) {
+        const projectDiv = document.getElementById(projectId);
+        const projecterr = document.getElementById('#hide');
+        // projecterr.style.display = "none"
+
+        if (projectDiv) {
+            projectDiv.style.display = "block";
+            projecterr.style.display = "none";
+        }
+    }
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function goToDetails(){
+//     window.location.href = 'details.html?project=' + projectId;
+// }
+
+// function getQueryParams(param){
+//     const UrlParams = new URLSearchParams(window.location.search);
+//     return UrlParams.get(param);
+// }
+// const projectId = getQueryParams("project");
+
+// if(projectId) {
+//   const projectDiv = document.getElementById(projectId);
+//     if (projectDiv){
+//       projectDiv.style.display = "block";
+//   }
+// }
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
